5308946282eeb195f11645b818004a5cfb61ec1d
Burak Enez <burakenez@gmail.com>
2026-05-08 17:01:31 +0300

    Update GD32F30x CMake projects [2026-05-08 17:01]

---
+++ Examples/ADC/ADC0_temperature_Vref/Application/Core/Src/main.c
@@ -0,0 +1,171 @@
+/*!
+    \file    main.c
+    \brief   ADC channel of temperature and Vref 
+
+    \version 2026-2-6, V3.0.3, firmware for GD32F30x
+*/
+
+/*
+    Copyright (c) 2025, GigaDevice Semiconductor Inc.
+
+    Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this 
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice, 
+       this list of conditions and the following disclaimer in the documentation 
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors 
+       may be used to endorse or promote products derived from this software without 
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
+OF SUCH DAMAGE.
+*/
+
+#include "gd32f30x.h"
+#include "systick.h"
+#include <stdio.h>
+#include "gd32f307c_eval.h"
+
+float temperature;
+float vref_value;
+uint16_t adc_value[2];
+
+void rcu_config(void);
+void dma_config(void);
+void adc_config(void);
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* configure systick */
+    systick_config();
+    /* system clocks configuration */
+    rcu_config();
+    /* DMA configuration */
+    dma_config();
+    /* ADC configuration */
+    adc_config();
+    /* USART configuration */
+    gd_eval_com_init(EVAL_COM0);
+
+    while(1){
+        /* delay a time in milliseconds */
+        delay_1ms(2000);
+
+        /* value convert */
+        temperature = (1.43 - adc_value[0]*3.3/4096) * 1000 / 4.3 + 25;
+        vref_value = (adc_value[1] * 3.3 / 4096);
+
+        /* value print */
+        printf(" the temperature data is %2.0f degrees Celsius\r\n", temperature);
+        printf(" the reference voltage data is %5.3fV \r\n", vref_value);
+        printf(" \r\n");
+    }
+}
+
+/*!
+    \brief      configure the different system clocks
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void rcu_config(void)
+{
+    /* enable ADC clock */
+    rcu_periph_clock_enable(RCU_ADC0);
+    /* config ADC clock */
+    rcu_adc_clock_config(RCU_CKADC_CKAPB2_DIV4);
+    /* enable DMA0 clock */
+    rcu_periph_clock_enable(RCU_DMA0);
+}
+
+/*!
+    \brief      configure the DMA peripheral
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void dma_config(void)
+{
+    /* ADC_DMA_channel configuration */
+    dma_parameter_struct dma_data_parameter;
+
+    /* ADC DMA_channel configuration */
+    dma_deinit(DMA0, DMA_CH0);
+
+    /* initialize DMA single data mode */
+    dma_data_parameter.periph_addr  = (uint32_t)(&ADC_RDATA(ADC0));
+    dma_data_parameter.periph_inc   = DMA_PERIPH_INCREASE_DISABLE;
+    dma_data_parameter.memory_addr  = (uint32_t)(&adc_value);
+    dma_data_parameter.memory_inc   = DMA_MEMORY_INCREASE_ENABLE;
+    dma_data_parameter.periph_width = DMA_PERIPHERAL_WIDTH_16BIT;
+    dma_data_parameter.memory_width = DMA_MEMORY_WIDTH_16BIT;
+    dma_data_parameter.direction    = DMA_PERIPHERAL_TO_MEMORY;
+    dma_data_parameter.number       = 2;
+    dma_data_parameter.priority     = DMA_PRIORITY_HIGH;
+    dma_init(DMA0, DMA_CH0, &dma_data_parameter);
+
+    dma_circulation_enable(DMA0, DMA_CH0);
+
+    /* enable DMA channel */
+    dma_channel_enable(DMA0, DMA_CH0);
+}
+
+/*!
+    \brief      configure the ADC peripheral
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void adc_config(void)
+{
+    /* ADC mode config */
+    adc_mode_config(ADC_MODE_FREE);
+    /* ADC contineous function disable */
+    adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, ENABLE);
+    /* ADC scan mode disable */
+    adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE);
+    /* ADC data alignment config */
+    adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);
+
+    /* ADC channel length config */
+    adc_channel_length_config(ADC0, ADC_ROUTINE_CHANNEL, 2);
+    /* ADC temperature sensor channel config */
+    adc_routine_channel_config(ADC0, 0, ADC_CHANNEL_16, ADC_SAMPLETIME_239POINT5);
+    /* ADC internal reference voltage channel config */
+    adc_routine_channel_config(ADC0, 1, ADC_CHANNEL_17, ADC_SAMPLETIME_239POINT5);
+
+    /* ADC trigger config */
+    adc_external_trigger_source_config(ADC0, ADC_ROUTINE_CHANNEL, ADC0_1_2_EXTTRIG_ROUTINE_NONE);
+    adc_external_trigger_config(ADC0, ADC_ROUTINE_CHANNEL, ENABLE);
+
+    /* ADC temperature and Vrefint enable */
+    adc_tempsensor_vrefint_enable();
+
+    /* ADC DMA function enable */
+    adc_dma_mode_enable(ADC0);
+    /* enable ADC interface */
+    adc_enable(ADC0);
+    delay_1ms(1);
+    /* ADC calibration and reset calibration */
+    adc_calibration_enable(ADC0);
+
+    /* ADC software trigger enable */
+    adc_software_trigger_enable(ADC0, ADC_ROUTINE_CHANNEL);
+}
